b2e1170bd8c6a5a291259a9bcdff13695a8701ff
Manikanta-81 <vaddimanikanta2002@gmail.com>
2025-03-03 15:18:39 +0530

    script.js file added and index.html and styles.css file updated

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBoxes = document.querySelectorAll(".menu-box");
+  
+    function revealOnScroll() {
+      const triggerPoint = window.innerHeight * 0.85; 
+      menuBoxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerPoint) {
+          box.classList.add("show");
+        }
+      });
+    }
+  
+    window.addEventListener("scroll", revealOnScroll);
+    revealOnScroll(); 
+  });
+  
